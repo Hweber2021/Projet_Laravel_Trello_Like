a241b31dec0df4fc391b557542a03ae7810c3082
Hugo Weber <alss-cdaalt21-whu@ccicampus.fr>
2022-03-02 17:35:28 +0100

    commit page d'acceuil début programmation

---
+++ resources/views/Homepage/homepage.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', 'Homepage')
+
+@section('style')
+
+    <style>
+
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+    </style>
+
+@endsection
+
+@section('content')
+
+@endsection
+
