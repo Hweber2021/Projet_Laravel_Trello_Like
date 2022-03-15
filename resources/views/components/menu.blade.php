d22df428b1c84c341231c270d437df544dc2f9e1
Hugo Weber <alss-cdaalt21-whu@ccicampus.fr>
2022-03-15 13:15:46 +0100

    commit branche + layout

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,2 +1,37 @@
-<nav>
-</nav>
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+
+                    <div class="login_link">
+                        <!-- Authentication Links -->
+                        @guest
+                            <td class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Connexion') }}</a>
+                            </td>
+                            @if (Route::has('register'))
+                                <td class="nav-item">
+                                    <a class="nav-link" href="{{ route('signup') }}">{{ __('Inscription') }}</a>
+                                </td>
+                            @endif
+                        @else
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                                   {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        @endguest
+                    </div>
+            </div>
+        </nav>
