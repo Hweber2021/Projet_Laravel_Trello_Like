9099de54d8010f1b621a7a9d3694c3c82ba3f299
Hugo Weber <alss-cdaalt21-whu@ccicampus.fr>
2022-03-20 14:44:15 +0100

    commit style

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,35 +1,35 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
 
-                <div class="navbar-collapse" id="navbarSupportedContent">
-                    <div class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <td class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Connexion') }}</a>
-                            </td>
-                            @if (Route::has('register'))
-                                <td class="nav-item">
-                                    <a class="nav-link" href="{{ route('signup') }}">{{ __('Inscription') }}</a>
-                                </td>
-                            @endif
-                        @else
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                      {{ __('Deconnexion') }}
-                                </a>
+        <div class="navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <td class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Connexion') }}</a>
+                    </td>
+                    @if (Route::has('register'))
+                        <td class="nav-item">
+                            <a class="nav-link" href="{{ route('signup') }}">{{ __('Inscription') }}</a>
+                        </td>
+                    @endif
+                    @else
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Deconnexion') }}
+                        </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                                </div>
-                        @endguest
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </div>
-                </div>
+                @endguest
             </div>
-        </nav>
+        </div>
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     @yield('style')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div id="app">
