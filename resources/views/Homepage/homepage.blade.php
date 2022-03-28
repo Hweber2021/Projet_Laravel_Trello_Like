8c1c2b84a1918f7154ad602e7957389a69b1d0cb
Hugo Weber <alss-cdaalt21-whu@ccicampus.fr>
2022-03-28 18:21:54 +0200

    commit style homepage

--- resources/views/Homepage/homepage.blade.php
+++ resources/views/Homepage/homepage.blade.php
@@ -8,19 +8,19 @@
     <div class="flex-center position-ref full-height">
 
         <div class='title'>
-            <h1>Bienvenu sur BookYourWork l'environnement Kanban frendly du net</h1>
+            <h1>Bienvenu sur BookYourWork l'environnement Kanban frendly</h1>
         </div>
 
-        <div class='text-center'>
+        <div class='homepage-text-center'>
             <p>
-            Vous trouverez au sein de ce site web toutes les fonctionnalités vous permettant la création de tableau de type Kanban.<br>
-            Au sein de ce logiciel, une interface de creation de tableau type Trello vous attend. Ces tableaux permettront à n'importe quel groupe de travail d'organiser la mise en place de mleur projet.<br>
-            Afin de bénéficié des fonctionnalitées de notre application, la création d'un compte est néscéssaire. Une fois votre compte créer un message de confirmation vous sera envoyé.<br>
+            Vous trouverez au sein de ce site web toutes les fonctionnalités vous permettant la création de tableau de type Kanban.<br><br><br>
+            Une interface ludique permettra aux groupes de travail d'organiser la mise en place de leurs projets.<br><br><br>
+            Afin de bénéficier des fonctionnalitées de  l'application, la création d'un compte est néscéssaire.<br><br>
             </p>
         </div>
 
         <div class="image_center">
-            <img src="/image/image_center.png" alt="image center">
+            <img src="/image/image_center.png" alt="image center" width="450">
         </div>
     </div>
 
